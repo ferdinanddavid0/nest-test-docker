58d18774351b2a94102dcbe5e44c330b7ab531d9
David Ferdinand <91716319+ferdinanddavid0@users.noreply.github.com>
2021-11-01 15:10:59 +0700

    Test Push, updated app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Welcome to the USGP, in the Circuit of the Americas, in Austin, Texas';
+    return 'Hello World! Welcome to the USGP, in the Circuit of the Americas, in Austin, Texas for round 16 of the F1 World Championship';
   }
 }
